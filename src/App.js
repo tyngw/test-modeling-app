e8bd096146e2b38618443b825cd07fcbaff40bee
tyngw <wish_awayoflife@yahoo.co.jp>
2024-02-23 16:47:45 +0900

    リファクタリング

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { calculateNodeWidth } from './util/TextUtilities';
 import { useWindowSize, calculateCanvasSize } from './util/LayoutUtilities';
 import Node from './components/Node';
 import InputFields from './components/InputFields';
+import Button from '@mui/material/Button';
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
 import './App.css';
 
 function App() {
@@ -17,7 +20,7 @@ function App() {
 
   // ウィンドウサイズとviewBoxのステート
   const windowSize = useWindowSize();
-  const [viewBox, setViewBox] = useState(`0 0 ${windowSize.width} ${windowSize.height}`);
+  const [viewBox, setViewBox] = useState(`0 0 ${windowSize.width * 0.5} ${windowSize.height * 0.5}`);
   const lastDistanceRef = useRef(null); // 最後の距離を格納するためのref
 
   const nodeHeight = 60;
@@ -30,6 +33,14 @@ function App() {
   const [startPosition, setStartPosition] = useState({ x: 0, y: 0 });
   const [originalPosition, setOriginalPosition] = useState({ x: 0, y: 0 });
 
+  // Undo/Redoのためのノードのスナップショットを保存
+  const [snapshots, setSnapshots] = useState([]);
+  const [snapshotIndex, setSnapshotIndex] = useState(0);
+
+  // svgのズームレベルを管理するステート
+  const [zoomLevel, setZoomLevel] = useState(1);
+
+  // テキストフィールドの参照を作成
   const inputRefs = {
     text: useRef(null),
     text2: useRef(null),
@@ -39,13 +50,19 @@ function App() {
   // ノードがキャンバスサイズを超えた場合に備えて、キャンバスの最小サイズを設定する
   const [canvasSize, setCanvasSize] = useState({ width: windowSize.width, height: windowSize.height });
 
+  // 指定されたidを持つノードを取得する関数
   function getNodeById(nodes, id) {
     return nodes.find(node => node.id === id);
   }
 
-  // 選択中のノードを取得する関数
+  // 選択中のノードの一つ目を取得する関数
   function getSelectedNode(nodes) {
     return nodes.find(node => node.selected);
+  }
+
+  // 選択中のノードの全てを取得する関数
+  function getSelectedNodes(nodes) {
+    return nodes.filter(node => node.selected);
   }
 
   function adjustNodeAndChildrenPosition(node, currentY, allNodes, depthOffset = 260, ySpacing = 10) {
@@ -96,17 +113,23 @@ function App() {
     return sortedNodes; // 更新されたノードの配列を返却
   }, [nodeHeight]);
 
-  // Undo/Redoのためのノードのスナップショットを保存
-  const [snapshots, setSnapshots] = useState([]);
-  const [snapshotIndex, setSnapshotIndex] = useState(-1);
-
-  // ノードの追加・削除・移動などの変更があった場合にスナップショットを保存
-  useEffect(() => {
-    if (snapshotIndex === snapshots.length - 1) {
-      setSnapshots([...snapshots, nodes]);
-      setSnapshotIndex(snapshotIndex + 1);
+  // Undo処理を行う関数
+  const undo = () => {
+    if (snapshotIndex > 0) {
+      console.log(`Undo snapshotIndex: ${snapshotIndex}`);
+      setSnapshotIndex(snapshotIndex - 1);
+      setNodes(snapshots[snapshotIndex - 1]);
     }
-  }, [nodes]);
+  };
+
+  // Redo処理を行う関数
+  const redo = () => {
+    if (snapshotIndex < snapshots.length - 1) {
+      console.log(`Redo snapshotIndex: ${snapshotIndex}`);
+      setSnapshotIndex(snapshotIndex + 1);
+      setNodes(snapshots[snapshotIndex + 1]);
+    }
+  };
 
   // ドラッグ処理
   const handleMouseDown = (e, id) => {
@@ -156,14 +179,24 @@ function App() {
     // 新しいノードと既存のノードとの間で重なりをチェックし、調整
     let adjustedNodes = adjustNodePositions(newNodes)
 
-    // 状態を更新
-    setNodes(adjustedNodes);
+    return adjustedNodes;
   };
 
-  // 与えられたノードの削除処理を行う関数
-  // 引数としてノードのリストと削除対象のノードを受け取る(ノードのリストの名称はグローバルで定義しているnodes以外の名称にする)
+  // deleteNodeRecursive関数を呼び出す関数
+  // 引数としてノードのリストと削除対象のノードを受け取る
   const deleteNode = (nodeList, nodeToDelete) => {
-    
+    const updatedNodes = deleteNodeRecursive(nodeList, nodeToDelete);
+    return updatedNodes;
+  }
+
+  // 与えられたノードを再帰的に削除する関数
+  // 引数としてノードのリストと削除対象のノードを受け取る
+  const deleteNodeRecursive = (nodeList, nodeToDelete) => {
+
+    // 与えられたnodeToDeleteのparentIdがnullの場合は処理を終了
+    if (nodeToDelete.parentId === null) {
+      return nodeList;
+    }
     // 指定されたノードを除外して新しいノードのリストを作成
     let updatedNodes = nodeList.filter(node => node.id !== nodeToDelete.id);
 
@@ -173,7 +206,7 @@ function App() {
     if (childNodes.length > 0) {
       childNodes.forEach(childNode => {
         console.log(`削除対象の子ノードのtext: ${childNode.text}`);
-        updatedNodes = deleteNode(updatedNodes, childNode);
+        updatedNodes = deleteNodeRecursive(updatedNodes, childNode);
       });
     }
 
@@ -189,27 +222,91 @@ function App() {
     return updatedNodes;
   };
 
+  const switchSelectedNode = (selectedNodeId) => {
+    if (selectedNodeId !== null && selectedNodeId !== undefined) {
+      setNodes(nodes.map(node => ({
+        ...node,
+        selected: node.id === selectedNodeId,
+      })));
+    }
+  };
+
+  const handleArrowUp = () => {
+      const selectedNode = nodes.find(node => node.selected);
+      if (!selectedNode) return;
+
+      const siblingNodes = nodes.filter(node => node.parentId === selectedNode.parentId);
+      const currentIndex = siblingNodes.findIndex(node => node.id === selectedNode.id);
+      if (currentIndex > 0) {
+        switchSelectedNode(siblingNodes[currentIndex - 1].id);
+      } else if (selectedNode.parentId !== null) {
+        // 親のノードの末尾のノードを選択
+        const parentNode = getNodeById(nodes, selectedNode.parentId);
+        const parentSiblingNodes = nodes.filter(node => node.parentId === parentNode.parentId);
+        const parentIndex = parentSiblingNodes.findIndex(node => node.id === parentNode.id);
+        if (parentIndex > 0) {
+          const lastChildOfPreviousParent = nodes.filter(node => node.parentId === parentSiblingNodes[parentIndex - 1].id).slice(-1)[0];
+          if (lastChildOfPreviousParent) selectNode(lastChildOfPreviousParent.id);
+        }
+      }
+    };
+
+  const handleArrowDown = () => {
+    const selectedNode = nodes.find(node => node.selected);
+    if (!selectedNode) return;
+
+    const siblingNodes = nodes.filter(node => node.parentId === selectedNode.parentId);
+    const currentIndex = siblingNodes.findIndex(node => node.id === selectedNode.id);
+    if (currentIndex < siblingNodes.length - 1) {
+      switchSelectedNode(siblingNodes[currentIndex + 1].id);
+    } else if (selectedNode.parentId !== null) {
+      // 次の親ノードの先頭のノードを選択
+      const parentNode = getNodeById(nodes, selectedNode.parentId);
+      const parentSiblingNodes = nodes.filter(node => node.parentId === parentNode.parentId);
+      const parentIndex = parentSiblingNodes.findIndex(node => node.id === parentNode.id);
+      if (parentIndex < parentSiblingNodes.length - 1) {
+        const firstChildOfNextParent = nodes.filter(node => node.parentId === parentSiblingNodes[parentIndex + 1].id)[0];
+        if (firstChildOfNextParent) selectNode(firstChildOfNextParent.id);
+      }
+    }
+  };
+
+  const findNodeAndSwitch = (conditionCallback) => {
+    const selectedNode = nodes.find(node => node.selected);
+    if (!selectedNode) return;
+
+    const newSelectedNode = nodes.find(conditionCallback);
+    if (newSelectedNode) {
+      switchSelectedNode(newSelectedNode.id);
+    }
+  };
+
   useEffect(() => {
     const handleKeyDown = (event) => {
-      // キー操作による新しいノードの追加処理
-      // 関数化して、useEffectの外に出す
+      // キー操作による新しいノードの追加処理(タブキーが押された場合, 選択中のノードがある場合, 編集モードでない場合)
       if (event.key === 'Tab' && nodes.some(node => node.selected) && editingId === null) {
-        // preventDefault()を呼び出して、ブラウザのデフォルトの挙動をキャンセル
-        event.preventDefault();
+        event.preventDefault(); // ブラウザデフォルトの挙動を防止
         const selectedNode = getSelectedNode(nodes);
 
-        // 新しいノードを追加(addNode関数を使って追加処理を行う)
-        addNode(selectedNode);
+        // Undoのスナップショットを追加
+        setSnapshots([...snapshots.slice(0, snapshotIndex + 1), nodes]);
+        setSnapshotIndex(snapshotIndex + 1);
+        console.log(`snapshotIndex: ${snapshotIndex}`);
+
+
+        // 新しいノードを追加する処理
+        let updatedNodes = addNode(selectedNode);
+        setNodes(updatedNodes);
       }
 
       if (editingId !== null && event.key === 'Tab') {
-        event.preventDefault(); // デフォルトの挙動を防止
+        event.preventDefault(); // ブラウザデフォルトの挙動を防止
         const fields = ['text', 'text2', 'text3'];
         const currentIndex = fields.indexOf(editingField);
         const nextIndex = (currentIndex + 1) % fields.length;
         setEditingField(fields[nextIndex]);
 
-        // `text3`からTab/Enterを押した場合、編集モードを終了
+        // `text3`からTabを押した場合、編集モードを終了
         if (editingField === 'text3') {
           setEditingId(null);
         } else {
@@ -221,6 +318,21 @@ function App() {
         }
       }
 
+      // Ctrl + ZでUndo処理
+      // Macの場合は、Command + ZでUndo処理
+      if ((event.ctrlKey || event.metaKey) && event.key === 'z') {
+        event.preventDefault();
+        //undo関数を呼び出してUndo処理を行う
+        undo();
+      }
+
+      // Shift + Ctrl + ZでRedo処理
+      // Macの場合は、Shift + Command + ZでRedo処理
+      if ((event.ctrlKey || event.metaKey) && event.shiftKey && event.key === 'z') {
+        event.preventDefault();
+        redo();
+      }
+
       if (event.key === 'Enter' && editingId === null && nodes.some(node => node.selected)) {
         // Enterキーが押され、選択中のノードがある場合、編集モードに入る
         const selectedNode = nodes.find(node => node.selected);
@@ -228,6 +340,10 @@ function App() {
       } else if (editingId === null && nodes.some(node => node.selected)) {
         if (event.key === 'Delete' || event.key === 'Backspace') {
           const selectedNodeIds = nodes.filter(node => node.selected).map(node => node.id);
+          // Undoのスナップショットを追加
+          setSnapshots([...snapshots.slice(0, snapshotIndex + 1), nodes]);
+          setSnapshotIndex(snapshotIndex + 1);
+
           // 選択されたノードを削除する処理
           let updatedNodes = nodes;
           selectedNodeIds.forEach(id => {
@@ -237,65 +353,9 @@ function App() {
         }
       }
 
-      // ノード選択処理
+      // ノードの選択処理
       if (nodes.some(node => node.selected)) {
-        const switchSelectedNode = (selectedNodeId) => {
-          if (selectedNodeId !== null && selectedNodeId !== undefined) {
-            setNodes(nodes.map(node => ({
-              ...node,
-              selected: node.id === selectedNodeId,
-            })));
-          }
-        };
-
-        const findNodeAndSwitch = (conditionCallback) => {
-          const selectedNode = nodes.find(node => node.selected);
-          if (!selectedNode) return;
-
-          const newSelectedNode = nodes.find(conditionCallback);
-          if (newSelectedNode) {
-            switchSelectedNode(newSelectedNode.id);
-          }
-        };
-        const handleArrowUp = () => {
-          const selectedNode = nodes.find(node => node.selected);
-          if (!selectedNode) return;
-
-          const siblingNodes = nodes.filter(node => node.parentId === selectedNode.parentId);
-          const currentIndex = siblingNodes.findIndex(node => node.id === selectedNode.id);
-          if (currentIndex > 0) {
-            switchSelectedNode(siblingNodes[currentIndex - 1].id);
-          } else if (selectedNode.parentId !== null) {
-            // 親のノードの末尾のノードを選択
-            const parentNode = getNodeById(nodes, selectedNode.parentId);
-            const parentSiblingNodes = nodes.filter(node => node.parentId === parentNode.parentId);
-            const parentIndex = parentSiblingNodes.findIndex(node => node.id === parentNode.id);
-            if (parentIndex > 0) {
-              const lastChildOfPreviousParent = nodes.filter(node => node.parentId === parentSiblingNodes[parentIndex - 1].id).slice(-1)[0];
-              if (lastChildOfPreviousParent) selectNode(lastChildOfPreviousParent.id);
-            }
-          }
-        };
-
-        const handleArrowDown = () => {
-          const selectedNode = nodes.find(node => node.selected);
-          if (!selectedNode) return;
-
-          const siblingNodes = nodes.filter(node => node.parentId === selectedNode.parentId);
-          const currentIndex = siblingNodes.findIndex(node => node.id === selectedNode.id);
-          if (currentIndex < siblingNodes.length - 1) {
-            switchSelectedNode(siblingNodes[currentIndex + 1].id);
-          } else if (selectedNode.parentId !== null) {
-            // 次の親ノードの先頭のノードを選択
-            const parentNode = getNodeById(nodes, selectedNode.parentId);
-            const parentSiblingNodes = nodes.filter(node => node.parentId === parentNode.parentId);
-            const parentIndex = parentSiblingNodes.findIndex(node => node.id === parentNode.id);
-            if (parentIndex < parentSiblingNodes.length - 1) {
-              const firstChildOfNextParent = nodes.filter(node => node.parentId === parentSiblingNodes[parentIndex + 1].id)[0];
-              if (firstChildOfNextParent) selectNode(firstChildOfNextParent.id);
-            }
-          }
-        };
+        // 矢印キーによるノードの選択処理
         switch (event.key) {
           case 'ArrowLeft':
             findNodeAndSwitch(node => node.id === nodes.find(n => n.selected).parentId);
@@ -318,7 +378,7 @@ function App() {
 
     document.addEventListener('keydown', handleKeyDown);
     return () => document.removeEventListener('keydown', handleKeyDown);
-  }, [nodes, editingId, editingField]);
+  }, [nodes, editingId, editingField, snapshots, snapshotIndex, undo, redo]);
 
   useEffect(() => {
     if (editingId !== null) {
@@ -430,8 +490,8 @@ function App() {
     setCanvasSize(calculateCanvasSize(nodes, calculateNodeWidth, 50, 200, windowSize));
   }, [nodes, windowSize]);
 
-  // 2点間の距離を計算するロジックをここに記述...
-  // この距離の変化に基づいてズームレベルを更新...
+
+  // ズームレベルに基づいてviewBoxを更新...
   useEffect(() => {
     const handleTouchMove = (event) => {
       if (event.touches.length === 2) {
@@ -483,8 +543,8 @@ function App() {
     }));
   };
 
+  // ノードの選択処理
   const selectNode = (id) => {
-    // const selectedNode = nodes.find(node => node.id === id);
     const selectedNode = getNodeById(nodes, id);
     console.log(`Selected Node: id=${selectedNode.id}, x=${selectedNode.x}, y=${selectedNode.y}, parentId=${selectedNode.parentId}, order=${selectedNode.order}, children=${selectedNode.children}`);
 
@@ -498,33 +558,50 @@ function App() {
 
   return (
     <div className="App" style={{ width: '200%', height: '200%', overflow: 'auto' }}>
-      <svg
-        width={canvasSize.width * 2}
-        height={canvasSize.height * 2}
-        onClick={handleClickOutside}
-        style={{ touchAction: 'none' }} // ブラウザのデフォルトのピンチズームを無効化
-      >
-        {nodes.map((node) => (
-          <Node
-            key={node.id}
-            node={node}
-            getNodeById={getNodeById}
-            calculateNodeWidth={calculateNodeWidth}
-            nodeHeight={nodeHeight}
-            curveControlOffset={curveControlOffset}
-            arrowOffset={arrowOffset}
-            selectNode={selectNode}
-            handleDoubleClick={handleDoubleClick}
-            handleMouseDown={handleMouseDown}
-            nodes={nodes}
-          />
-        ))}
-        <defs>
-          <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="0" refY="3.5" orient="auto" fill="none" stroke="black">
-            <polygon points="0 0, 10 3.5, 0 7" fill="none" stroke="black" />
-          </marker>
-        </defs>
-      </svg>
+      {/* // アイコンバーを表示。背景灰色 */}
+      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '40px', backgroundColor: 'lightgray' }}>
+        <div style={{ display: 'flex', justifyContent: 'left', alignItems: 'center', height: '100%' }}>
+          <Button variant="contained" onClick={undo}>
+            <UndoIcon />
+            Undo
+          </Button>
+          <div style={{ width: '20px' }}></div>
+          <Button variant="contained" onClick={redo}>
+            <RedoIcon />
+            Redo
+          </Button>
+        </div>
+      </div>
+      {/* // Undo/Redoボタンを左上に配置 */}
+      <div style={{ position: 'absolute', top: '40px', left: 0 }}>
+        <svg
+          width={canvasSize.width * 2}
+          height={canvasSize.height * 2}
+          onClick={handleClickOutside}
+          style={{ touchAction: 'none' }} // ブラウザのデフォルトのピンチズームを無効化
+        >
+          {nodes.map((node) => (
+            <Node
+              key={node.id}
+              node={node}
+              getNodeById={getNodeById}
+              calculateNodeWidth={calculateNodeWidth}
+              nodeHeight={nodeHeight}
+              curveControlOffset={curveControlOffset}
+              arrowOffset={arrowOffset}
+              selectNode={selectNode}
+              handleDoubleClick={handleDoubleClick}
+              handleMouseDown={handleMouseDown}
+              nodes={nodes}
+            />
+          ))}
+          <defs>
+            <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="0" refY="3.5" orient="auto" fill="none" stroke="black">
+              <polygon points="0 0, 10 3.5, 0 7" fill="none" stroke="black" />
+            </marker>
+          </defs>
+        </svg>
+      </div>
       <InputFields node={editingNode} updateText={updateText} editingField={editingField} />
     </div >
   );
