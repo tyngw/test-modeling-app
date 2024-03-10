c065d52e2027a904a461d6e90d8e3a0a5241a0c6
tyngw <wish_awayoflife@yahoo.co.jp>
2024-03-10 15:34:31 +0900

    refactor: NodeActionHelperに分離

--- src/state/state.js
+++ src/state/state.js
@@ -7,13 +7,19 @@ import {
     NODE_HEIGHT,
     MIN_WIDTH
 } from '../constants/Node';
+import { 
+    deleteNodeRecursive, 
+    setDepthRecursive, 
+    getSelectedNodeAndChildren, 
+    pasteNodes, 
+    setVisibilityRecursive
+} from '../utils/NodeActionHelper';
 
 const createNewNode = (parentId, order, depth) => ({
     id: 0, // この値は後で上書きされます
     text: '',
     text2: '',
     text3: '',
-    selected: false,
     x: 0,
     y: 0,
     width: MIN_WIDTH,
@@ -42,7 +48,6 @@ const initialState = {
 };
 
 const addNode = (allNodes, parentNode) => {
-    let newNodes;
     const newId = Math.max(...allNodes.map(node => node.id), 0) + 1;
     const newOrder = parentNode.children;
     const newRect = {
@@ -59,121 +64,6 @@ const addNode = (allNodes, parentNode) => {
     return [...updatedNodes, newRect];
 };
 
-const getSelectedNodeAndChildren = (nodeList, targetNode, selectedNode) => {
-    let cutNodes = [];
-
-
-    if (targetNode.id === selectedNode.id) {
-        cutNodes.push({ ...targetNode, parentId: null });
-    } else {
-        cutNodes.push(targetNode);
-    }
-
-    // 選択対象のノードIdと一致するparentIdを持つノードを再帰的に取得
-    const childNodes = nodeList.filter(node => node.parentId === targetNode.id);
-    if (childNodes.length > 0) {
-        childNodes.forEach(childNode => {
-            cutNodes = [...cutNodes, ...getSelectedNodeAndChildren(nodeList, childNode, selectedNode)];
-        });
-    }
-
-    return cutNodes;
-};
-
-// cutNodesに含まれるノードをnodesに追加できるように新しいノードを作成する関数
-const pasteNodes = (nodeList, cutNodes, parentNode) => {
-    const rootNode = cutNodes.find(node => node.parentId === null);
-    if (!rootNode) {
-        return [...nodeList, ...cutNodes]
-    }
-    const rootNodeDepth = rootNode.depth;
-    const baseDepth = parentNode.depth + 1;
-    const depthDelta = baseDepth - rootNodeDepth;
-    let newId = Math.max(...nodeList.map(node => node.id), 0) + 1;
-    const idMap = new Map();
-
-    const newNodes = cutNodes.map(cutNode => {
-        if (nodeList.find(node => node.id === cutNode.id)) {
-            idMap.set(cutNode.id, newId);
-            cutNode.id = newId;
-            newId++;
-        }
-        cutNode.depth = cutNode.depth + depthDelta;
-        
-        if (cutNode.id === rootNode.id) {
-            cutNode.parentId = parentNode.id;
-        }
-
-        return cutNode;
-    });
-
-    const updatedNodes = nodeList.concat(newNodes.map(node => {
-        if (idMap.has(node.parentId)) {
-            node.parentId = idMap.get(node.parentId);
-        }
-        return node;
-    }));
-
-    return updatedNodes;
-}
-
-
-// 指定されたノードの子ノードのdepthを再帰的に親ノードのdepth+1に設定する関数
-const setDepthRecursive = (nodeList, parentNode) => {
-    return nodeList.reduce((updatedNodes, node) => {
-        let newNode = node;
-        if (node.parentId === parentNode.id) {
-            newNode = { ...node, depth: parentNode.depth + 1 };
-            updatedNodes = setDepthRecursive(updatedNodes, newNode);
-        }
-        return [...updatedNodes, newNode];
-    }, []);
-};
-
-// 指定されたノードの子ノードのvisibleを再帰的にtrueまたはfalseに設定する関数
-// 戻り値として、visibleを更新した新しいノードのリストを返す
-const setVisibilityRecursive = (nodeList, parentNode, visible) => {
-    return nodeList.reduce((updatedNodes, node) => {
-        let newNode = node;
-        if (node.parentId === parentNode.id) {
-            newNode = { ...node, visible: visible };
-            updatedNodes = setVisibilityRecursive(updatedNodes, newNode, visible);
-        }
-        return [...updatedNodes, newNode];
-    }, []);
-};
-
-// 与えられたノードを再帰的に削除する関数
-// 引数としてノードのリストと削除対象のノードを受け取る
-const deleteNodeRecursive = (nodeList, nodeToDelete) => {
-
-    // 与えられたnodeToDeleteのparentIdがnullの場合は処理を終了
-    if (nodeToDelete.parentId === null) {
-        return nodeList;
-    }
-    // 指定されたノードを除外して新しいノードのリストを作成
-    let updatedNodes = nodeList.filter(node => node.id !== nodeToDelete.id);
-
-    // 削除対象のノードIdと一致するparentIdを持つノードも削除する
-    // 再起的に自身のdeleteNode関数を呼び出して処理する
-    const childNodes = updatedNodes.filter(node => node.parentId === nodeToDelete.id);
-    if (childNodes.length > 0) {
-        childNodes.forEach(childNode => {
-            updatedNodes = deleteNodeRecursive(updatedNodes, childNode);
-        });
-    }
-
-    // 指定されたノードのIdと一致するparentIdを持つノードのchildrenをデクリメント
-    updatedNodes = updatedNodes.map(node => {
-        if (nodeToDelete.parentId === node.id) {
-            return { ...node, children: node.children - 1 };
-        }
-        return node;
-    });
-
-    return updatedNodes;
-};
-
 function updateChildren(nodes, id, increment) {
     return nodes.map(node => {
         if (node.id === id) {
@@ -188,7 +78,6 @@ function reducer(state, action) {
     const selectedNode = state.nodes.find(node => node.selected);
 
     switch (action.type) {
-        // initialStateを返す
         case 'NEW':
             clearSnapshots();
             return initialState;
@@ -246,7 +135,6 @@ function reducer(state, action) {
             } else {
                 return state;
             }
-
         case 'END_EDITING':
             // 編集中のフィールドを終了する
             updatedNodes = adjustNodePositions(state.nodes);
@@ -255,10 +143,6 @@ function reducer(state, action) {
             return { ...state, nodes: Undo(state.nodes) };
         case 'REDO':
             return { ...state, nodes: Redo(state.nodes) };
-        case 'ZOOM_IN':
-            return { ...state, zoomRatio: state.zoomRatio + 0.1 };
-        case 'ZOOM_OUT':
-            return { ...state, zoomRatio: state.zoomRatio - 0.1 };
         case 'SNAPSHOT':
             saveSnapshot(state.nodes);
             return state;
