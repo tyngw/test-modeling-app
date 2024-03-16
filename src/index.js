07a18324c95ead0855d0d4533827886ace98e972
tyngw <wish_awayoflife@yahoo.co.jp>
2024-03-16 18:00:04 +0900

    Fix: Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactGA.initialize("G-CVYKPR22E4");
-ReactGA.send("pageview");
+function MyApp() {
+  useEffect(() => {
+    ReactGA.initialize("G-CVYKPR22E4");
+    ReactGA.send("pageview");
+  }, []);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<MyApp />);
