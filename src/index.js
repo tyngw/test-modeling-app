c9d96176868541c65200f0432f6f54365b8613ec
tyngw <env.dev.tk@gmail.com>
2024-03-16 17:50:02 +0900

    Add: Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactGA.initialize("G-CVYKPR22E4");
+ReactGA.send("pageview");
+
 root.render(
   <React.StrictMode>
     <App />
