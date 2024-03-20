d743823703a539ccd4f54d28e05bb193e19b8ea1
tyngw <env.dev.tk@gmail.com>
2024-03-20 18:42:34 +0900

    Fix: message for mobile browser

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import ViewBox from './components/ViewBox';
 import './App.css';
+import { isMobileDevice } from './utils/DeviceChecker';
 
 function App() {
   return (
     <div className="App">
+        {isMobileDevice() && <div style={{backgroundColor: '#006666', color: 'white', textAlign: 'center'}}>このアプリはモバイルデバイスに対応していません。</div>}
         <ViewBox/>
     </div>
   );
