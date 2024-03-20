3ad669980b689f3a668a889af473b28762767ad8
tyngw <env.dev.tk@gmail.com>
2024-03-20 21:18:46 +0900

    Revert "fix #2"
    This reverts commit 913636b14080695bef6b92039b872cd4b8f9085f.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // App.js
 import React from 'react';
 import ViewBox from './components/ViewBox';
-import './App.css'
+import './App.css';
+import { isMobileDevice } from './utils/DeviceChecker';
 
 function App() {
   return (
     <div className="App">
+        {isMobileDevice() && <div style={{backgroundColor: '#006666', color: 'white', textAlign: 'center'}}>このアプリはモバイルデバイスに対応していません。</div>}
         <ViewBox/>
     </div>
   );
