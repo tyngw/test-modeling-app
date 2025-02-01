86786e15cdf522de1641d1ab413f02fdb1744839
tyngw <wish_awayoflife@yahoo.co.jp>
2025-02-02 00:09:02 +0900

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('view-areaが表示されていることを確認', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const viewArea = screen.getByTestId('view-area');
+  expect(viewArea).toBeInTheDocument();
 });
