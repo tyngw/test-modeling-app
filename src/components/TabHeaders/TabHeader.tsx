788dbb61c1c79a8344a4696a01e482f82f6ee786
tyngw <env.dev.tk@gmail.com>
2025-02-22 07:37:18 +0900

    Component separation

---
+++ src/components/TabHeaders/TabHeader.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { TabState } from '../../context/TabsContext';
+
+interface TabHeaderProps {
+  tab: TabState;
+  isCurrent: boolean;
+  closeTab: (id: string) => void;
+  switchTab: (id: string) => void;
+}
+
+const TabHeader: React.FC<TabHeaderProps> = React.memo(({ 
+  tab, 
+  isCurrent, 
+  closeTab, 
+  switchTab 
+}) => (
+  <div
+    style={{
+      padding: '8px',
+      marginRight: '4px',
+      backgroundColor: isCurrent ? '#fff' : '#ddd',
+      borderBottom: isCurrent ? 'solid 3px #87CEFA' : '',
+      paddingBottom: '3px',
+      borderRadius: '5px 5px 0 0',
+      fontSize: '12px',
+      cursor: 'pointer',
+      display: 'flex',
+      alignItems: 'center',
+    }}
+    onClick={() => switchTab(tab.id)}
+  >
+    <span>{tab.name}</span>
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        closeTab(tab.id);
+      }}
+      style={{ 
+        marginLeft: '8px',
+        border: '0px',
+        backgroundColor: 'transparent',
+        fontSize: '16px',
+        color: '#666666',
+        fontWeight: 'bold',
+        cursor: 'pointer',
+       }}
+    >
+      ×
+    </button>
+  </div>
+));
+
+export default TabHeader;
--- src/context/AppContent.tsx
+++ src/context/AppContent.tsx
@@ -10,7 +10,7 @@ import { saveSvg } from '../utils/FileHelpers';
 import { loadElements, saveElements } from '../utils/FileHelpers';
 import { ICONBAR_HEIGHT, TABBAR_HEIGHT } from '../constants/ElementSettings';
 import { TabState } from './TabsContext';
-import { getLastSavedFileName } from '../utils/FileHelpers';
+import TabHeaders from '../components/TabHeaders';
 
 const AppContent: React.FC = () => {
   const { tabs, currentTabId, addTab, closeTab, switchTab, updateTabState, updateTabName } = useTabs();
@@ -31,7 +31,6 @@ const AppContent: React.FC = () => {
           loadElements={(event) => loadElements(event.nativeEvent)
             .then(({ elements, fileName }) => {
               dispatch({ type: 'LOAD_ELEMENTS', payload: elements });
-              // ファイル名から拡張子を除いた名前でタブ名を更新する
               const newTabName = fileName.replace('.json', '');
               updateTabName(currentTabId, newTabName);
             })
@@ -57,79 +56,5 @@ const AppContent: React.FC = () => {
     </div>
   );
 };
-
-const TabHeaders: React.FC<{
-  tabs: TabState[];
-  currentTabId: string;
-  addTab: () => void;
-  closeTab: (id: string) => void;
-  switchTab: (id: string) => void;
-}> = React.memo(({ tabs, currentTabId, addTab, closeTab, switchTab }) => (
-  <div style={{
-    display: 'flex',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-    width: '100%',
-    height: TABBAR_HEIGHT,
-    marginTop: ICONBAR_HEIGHT,
-    position: 'fixed',
-    zIndex: 10001,
-  }}>
-    {tabs.map(tab => (
-      <TabHeader
-        key={tab.id}
-        tab={tab}
-        isCurrent={currentTabId === tab.id}
-        closeTab={closeTab}
-        switchTab={switchTab}
-      />
-    ))}
-    <button onClick={addTab} style={{ marginLeft: '8px' }}>
-      +
-    </button>
-  </div>
-));
-
-const TabHeader: React.FC<{
-  tab: TabState;
-  isCurrent: boolean;
-  closeTab: (id: string) => void;
-  switchTab: (id: string) => void;
-}> = React.memo(({ tab, isCurrent, closeTab, switchTab }) => (
-  <div
-    style={{
-      padding: '8px',
-      marginRight: '4px',
-      backgroundColor: isCurrent ? '#fff' : '#ddd',
-      borderBottom: isCurrent ? 'solid 3px #87CEFA' : '',
-      paddingBottom: '3px',
-      borderRadius: '5px 5px 0 0',
-      fontSize: '12px',
-      cursor: 'pointer',
-      display: 'flex',
-      alignItems: 'center',
-    }}
-    onClick={() => switchTab(tab.id)}
-  >
-    <span>{tab.name}</span>
-    <button
-      onClick={(e) => {
-        e.stopPropagation();
-        closeTab(tab.id);
-      }}
-      style={{ 
-        marginLeft: '8px',
-        border: '0px',
-        backgroundColor: 'transparent',
-        fontSize: '16px',
-        color: '#666666',
-        fontWeight: 'bold',
-        cursor: 'pointer',
-       }}
-    >
-      ×
-    </button>
-  </div>
-));
 
 export default AppContent;
