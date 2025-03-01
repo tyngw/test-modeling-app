36cbc094474d56fe42f565909e7ed297d1f1d147
tyngw <env.dev.tk@gmail.com>
2025-03-01 14:39:33 +0900

    Mobile compatible

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.tsx
 import React from 'react';
 import './App.css';
-import { isMobileDevice } from './utils/deviceChecker';
 import { TabsProvider } from './context/tabsContext';
 import { ToastProvider } from './context/toastContext';
 import AppContent from './appContent';
@@ -9,11 +8,6 @@ import AppContent from './appContent';
 function App() {
   return (
     <div className="App">
-      {isMobileDevice() && (
-        <div style={{ backgroundColor: '#006666', color: 'white', textAlign: 'center' }}>
-          このアプリはモバイルデバイスに対応していません。
-        </div>
-      )}
       <ToastProvider>
         <TabsProvider>
           <AppContent />
