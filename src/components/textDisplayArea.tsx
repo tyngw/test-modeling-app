b518c09ffeb54777f111f2ed40e030e7b21b0e78
tyngw <wish_awayoflife@yahoo.co.jp>
2025-03-18 22:49:07 +0900

    Change the method of arithmetic operations

--- src/components/textDisplayArea.tsx
+++ src/components/textDisplayArea.tsx
@@ -99,7 +99,7 @@ const TextDisplayArea: React.FC<TextDisplayAreaProps> = memo(({
           fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif`,
           width: `${dimensions.width - TEXTAREA_PADDING.HORIZONTAL}px`,
           minHeight: `${SIZE.SECTION_HEIGHT}px`,
-          padding: `${TEXTAREA_PADDING.VERTICAL / 2}px ${TEXTAREA_PADDING.HORIZONTAL / 2}px`,
+          padding: `${TEXTAREA_PADDING.VERTICAL * 0.5}px ${TEXTAREA_PADDING.HORIZONTAL * 0.5}px`,
           overflow: 'hidden',
           whiteSpace: 'pre-wrap',
           wordWrap: 'break-word',
