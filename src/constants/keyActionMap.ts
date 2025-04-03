de3a7c74e81fc4780559816b360743f564816c4b
tyngw <env.dev.tk@gmail.com>
2025-04-04 07:47:37 +0900

    Refactoring

--- src/constants/keyActionMap.ts
+++ src/constants/keyActionMap.ts
@@ -1,4 +1,5 @@
 // src/constants/keyActionMap.ts
+
 export const keyActionMap: { [key: string]: string } = {
     'Ctrl+z': 'UNDO',
     'Ctrl+Shift+z': 'REDO',
@@ -16,4 +17,12 @@ export const keyActionMap: { [key: string]: string } = {
     'Delete': 'DELETE_ELEMENT',
     'Backspace': 'DELETE_ELEMENT',
     'Enter': 'EDIT_ELEMENT'
+};
+
+export const inputFieldKeyActionMap: { [key: string]: string } = {
+    'Tab': 'NEXT_FIELD',
+    'Escape': 'END_EDITING',
+    'Ctrl+Enter': 'END_EDITING',
+    'Alt+Enter': 'END_EDITING',
+    'Meta+Enter': 'END_EDITING',
 };
